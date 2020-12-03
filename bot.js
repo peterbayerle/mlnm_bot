61e526c4a40500f826dab762c1b518a13b0797f4
Peter Bayerle <peterbayerle2@gmail.com>
2020-12-02 20:27:24 -0800

    initial bot setup + fun command

---
+++ bot.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const config = require('./config.json');
+require('dotenv').config();
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (file of commandFiles) {
+  const cmd = require(`./commands/${file}`);
+  client.commands.set(cmd.name, cmd);
+}
+
+client.on('message', msg => {
+	const args = msg.content.slice(config.prefix.length).trim().split(/ +/);
+	const cmd = args.shift().toLowerCase();
+
+  if (!client.commands.has(cmd)) return;
+
+  try {
+  	client.commands.get(cmd).execute(msg, args);
+  } catch (err) {
+  	console.error(err);
+  	msg.reply('there was an error trying to execute that command!');
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.BOT_TOKEN);
---
+++ commands/fun.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'branden-is',
+  description: 'model shares what Branden is to the world',
+  execute(msg, args) {
+    msg.channel.send('a dork');
+  }
+};
